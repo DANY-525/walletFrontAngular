9dfb45dd7e1a6d76657f18dbe8bf703c11ad5840
Daniel <dany525@gmail.com>
2025-05-23 16:27:09 -0500

    cambios en accounts

--- controlAccount/src/app/features/accounts/accounts.component.ts
+++ controlAccount/src/app/features/accounts/accounts.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Account } from './models/Account';
+import { CommonModule } from '@angular/common'; // ✅ importa esto
 
 @Component({
   selector: 'app-accounts',
-  imports: [],
+    standalone: true,
+  imports: [CommonModule], // ✅ habilita *ngFor y otras directivas comunes
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.css'
 })
 export class AccountsComponent {
+
+  listOfAccounts:Account[] =[];
+  accountBank: Account = {
+    id:"fasdfa",
+    name:"davivienda",
+    status:true
+  };
+
+  ngOnInit(){
+
+
+    this.listOfAccounts.push(this.accountBank);
+
+
+
+  }
+  
+
+
+
 
 }
